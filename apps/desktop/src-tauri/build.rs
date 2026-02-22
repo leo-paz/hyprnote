bf1a01304ddc5af7db766766f115d59fd56ceba5
Yujong Lee <yujonglee.dev@gmail.com>
2026-02-22 21:14:02 +0900

    comeback to static linking onnx runtime

--- apps/desktop/src-tauri/build.rs
+++ apps/desktop/src-tauri/build.rs
@@ -2,25 +2,5 @@ fn main() {
     #[cfg(target_os = "macos")]
     println!("cargo:rustc-link-arg=-fapple-link-rtlib");
 
-    #[cfg(debug_assertions)]
-    {
-        use std::path::PathBuf;
-
-        let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-
-        let dylib_name = if cfg!(target_os = "linux") {
-            "libonnxruntime.so"
-        } else if cfg!(target_os = "macos") {
-            "libonnxruntime.dylib"
-        } else {
-            ""
-        };
-
-        if !dylib_name.is_empty() {
-            let dylib_path = manifest_dir.join("resources").join(dylib_name);
-            println!("cargo:rustc-env=ORT_DYLIB_PATH={}", dylib_path.display());
-        }
-    }
-
     tauri_build::build()
 }
